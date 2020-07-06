b0d0b760af8000e32f4fa86ee650e8ba33d80bb0
gitee <djingw@sina.cn>
2020-07-06 17:58:10 +0800

    更新

---
+++ app/src/main/java/com/sai/robot/KeDaActivity.kt
@@ -0,0 +1,132 @@
+package com.sai.robot
+
+import android.os.Bundle
+import com.iflytek.cloud.RecognizerResult
+import com.iflytek.cloud.SpeechError
+import com.iflytek.cloud.WakeuperResult
+import com.sai.robot.iat.IATManager
+import com.sai.robot.iat.MyRecognizerListener
+import com.sai.robot.tts.MySynthesizerListener
+import com.sai.robot.tts.TTSManager
+import com.sai.robot.wake.MyWakeuperListener
+import com.sai.robot.wake.WakeManage
+import kotlinx.android.synthetic.main.activity_ke_da.*
+import org.json.JSONException
+import org.json.JSONObject
+class KeDaActivity : BaseActivity() {
+    lateinit var mWakeManager:WakeManage
+    lateinit var _iat:IATManager
+    lateinit var _tts:TTSManager
+    val bufferWake:StringBuffer= StringBuffer();
+    val bufferIAT:StringBuffer= StringBuffer();
+    var isStartIAT = false
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mWakeManager != null) {
+            mWakeManager.onDestroy()
+        }
+        if (_iat != null) {
+            _iat.onDestory()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ke_da)
+
+        // 唤醒
+        vWakeUp.setOnClickListener { startWake()}
+        //语音听写
+        vIAT.setOnClickListener { startIAT() }
+        vTTS.setOnClickListener { startTTS("山东飞机上飞机哦噻飞机上的减肥啦是的风景三闾大夫就是东方就死哦东方惊悚地方就是大家发来的科技发达") }
+
+    }
+    private fun startTTS(msg:String){
+            _tts = TTSManager()
+            _tts.builfTTs(this)
+
+        _tts.start(msg,object : MySynthesizerListener {
+            override fun onSpeakBegin() {
+                vTTS.setText("---开始阅读--")
+            }
+
+            override fun onSpeakProgress(percent: Int, beginPos: Int, endPos: Int) {
+                super.onSpeakProgress(percent, beginPos, endPos)
+                vTTS.setText("---阅读进度--${percent}")
+            }
+
+
+            override fun onCompleted(p0: SpeechError?) {
+                vTTS.setText("--结束-开始阅读--")
+            }
+
+        })
+    }
+
+    private fun startIAT(){
+         _iat = IATManager(this)
+        _iat.starIAT(object : MyRecognizerListener {
+
+            override fun onResult(results: RecognizerResult?, p1: Boolean) {
+                bufferIAT.append("----->>>>\n ${results!!.getResultString()} \n<<<<-----\n")
+                vIATRecord.setText(bufferIAT.toString())
+            }
+
+            override fun onBeginOfSpeech() {
+
+                bufferIAT.setLength(0)
+                bufferIAT.append("--开始听写--\n")
+                vIATRecord.setText(bufferIAT.toString())
+            }
+
+            override fun onEndOfSpeech() {
+                bufferIAT.append("--结束听写--\n\n")
+                vIATRecord.setText(bufferIAT.toString())
+            }
+        })
+    }
+
+    private fun startWake() {
+        mWakeManager = WakeManage(this)
+        mWakeManager.onStartWake(object : MyWakeuperListener {
+
+            override fun onBeginOfSpeech() {
+                bufferWake.setLength(0)
+                bufferWake.append("--开始听讲--\n")
+                vRecord.setText(bufferWake.toString())
+            }
+
+            override fun onResult(result: WakeuperResult?) {
+                if (result != null) {
+                    try {
+                        val res = JSONObject(result.resultString)
+                        bufferWake.append("---解析成功---\n")
+                        bufferWake.append("[RAW]: ${result.resultString}\n")
+                        bufferWake.append("[操作类型]: ${res.optString("sst")}\n")
+                        bufferWake.append("[唤醒词id]: ${res.optString("id")}\n")
+                        bufferWake.append("[得分]: ${res.optString("score")}\n")
+                        bufferWake.append("[前端点]: ${res.optString("bos")}\n")
+                        bufferWake.append("[尾端点]: ${res.optString("eos")}\n")
+                        bufferWake.append("---解析成功-本次结束--\n\n")
+                        vRecord.setText(bufferWake.toString())
+                    }catch (err: JSONException){
+                        bufferWake.append("---解析失败---\n")
+                        vRecord.setText(bufferWake.toString())
+                    }
+                }else{
+                    bufferWake.append("---监听结果失败---\n")
+                    vRecord.setText(bufferWake.toString())
+                }
+            }
+
+            override fun onError(error: SpeechError?) {
+                bufferWake.append("--监听失败--\n")
+                vRecord.setText(bufferWake.toString())
+                bufferWake.setLength(0)
+            }
+        })
+    }
+}
+
+
--- app/src/main/java/com/sai/robot/iat/IATManager.kt
+++ app/src/main/java/com/sai/robot/iat/IATManager.kt
@@ -8,43 +8,44 @@ import com.iflytek.cloud.SpeechConstant
 import com.iflytek.cloud.SpeechRecognizer
 
 class IATManager(context: Context) {
-val TAG = "-IATManager-"
+    val TAG = "-IATManager-"
     var mEngineType = SpeechConstant.TYPE_CLOUD
 
     val context = context
-    var   _mIat :SpeechRecognizer ? = null
+    lateinit var _mIat: SpeechRecognizer
 
-    fun starIAT(listener: RecognizerListener){
-       _mIat = SpeechRecognizer.createRecognizer(context, InitListener(){
-          Log.d(TAG,"语音识别器-初始化=${it}")
-      })
+    fun starIAT(listener: RecognizerListener) {
+        _mIat = SpeechRecognizer.createRecognizer(context, InitListener() {
+            Log.d(TAG, "语音识别器-初始化=${it}")
+        })
         //设置语法ID和 SUBJECT 为空，以免因之前有语法调用而设置了此参数；
         // 或直接清空所有参数，具体可参考 DEMO 的示例。
-        _mIat!!.setParameter( SpeechConstant.CLOUD_GRAMMAR, null )
-        _mIat!!.setParameter( SpeechConstant.SUBJECT, null )
+        _mIat.setParameter(SpeechConstant.CLOUD_GRAMMAR, null)
+        _mIat.setParameter(SpeechConstant.SUBJECT, null)
 //设置返回结果格式，目前支持json,xml以及plain 三种格式，其中plain为纯听写文本内容
-        _mIat!!.setParameter(SpeechConstant.RESULT_TYPE, "plain")
+        _mIat.setParameter(SpeechConstant.RESULT_TYPE, "plain")
 //此处engineType为“cloud”
-        _mIat!!.setParameter( SpeechConstant.ENGINE_TYPE, mEngineType )
+        _mIat.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType)
 //设置语音输入语言，zh_cn为简体中文
-        _mIat!!.setParameter(SpeechConstant.LANGUAGE, "zh_cn")
+        _mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn")
 //设置结果返回语言
-        _mIat!!.setParameter(SpeechConstant.ACCENT, "mandarin")
+        _mIat.setParameter(SpeechConstant.ACCENT, "mandarin")
 // 设置语音前端点:静音超时时间，单位ms，即用户多长时间不说话则当做超时处理
 //取值范围{1000～10000}
-        _mIat!!.setParameter(SpeechConstant.VAD_BOS, "4000")
+        _mIat.setParameter(SpeechConstant.VAD_BOS, "4000")
 //设置语音后端点:后端点静音检测时间，单位ms，即用户停止说话多长时间内即认为不再输入，
 //自动停止录音，范围{0~10000}
-        _mIat!!.setParameter(SpeechConstant.VAD_EOS, "1000")
+        _mIat.setParameter(SpeechConstant.VAD_EOS, "1000")
 //设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
-        _mIat!!.setParameter(SpeechConstant.ASR_PTT,"1")
+        _mIat.setParameter(SpeechConstant.ASR_PTT, "1")
 
         //开始识别，并设置监听器
-        _mIat!!.startListening(listener);
+        _mIat.startListening(listener);
+
     }
 
 
-    fun onStop(){
+    fun onStop() {
         if (_mIat == null) {
             return
         }
@@ -53,7 +54,7 @@ val TAG = "-IATManager-"
         }
     }
 
-    fun onDestory(){
+    fun onDestory() {
         if (_mIat == null) {
             return
         }
